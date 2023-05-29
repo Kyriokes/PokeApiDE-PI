4cb3e3843ce3ae93a81d5599b37438137c75694e
Sergio Ferrari <ferrari8986@gmail.com>
2023-05-28 23:48:48 -0300

    se agregan rutas y controladores 90%

--- api/src/routes/handlers/typeHandler.js
+++ api/src/routes/handlers/typeHandler.js
@@ -3,7 +3,7 @@ const { getTypes } = require("../controllers/typeController");
 const getTypeHandler = async (req, res) => {
   try {
     const types = await getTypes();
-    return res.send(types);
+    return res.json(types);
   } catch (error) {
     //manejo si hay un error
     return res.status(400).send({ error: error.message });
