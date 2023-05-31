91e588537171a205fb7cd9e9e118e9f158b25148
Sergio Ferrari <ferrari8986@gmail.com>
2023-05-31 04:53:44 -0300

    el back esta on fire

--- api/src/routes/controllers/pokemonController.js
+++ api/src/routes/controllers/pokemonController.js
@@ -9,7 +9,6 @@ function pokeData(poke) {
     image: poke.data.sprites.front_default,
     height: poke.data.height,
     weight: poke.data.weight,
-    types: poke.data.types.map((types) => types.type.name),
     hp: poke.data.stats.find((element) => element.stat.name === "hp").base_stat,
     attack: poke.data.stats.find((element) => element.stat.name === "attack")
       .base_stat,
@@ -17,18 +16,21 @@ function pokeData(poke) {
       .base_stat,
     speed: poke.data.stats.find((element) => element.stat.name === "speed")
       .base_stat,
+    types: poke.data.types.map((types) => types.type.name),
   };
 }
 
-function typeFilter(obj){ 
+function typeFilter(obj) {
+  // para filtrar el objeto en el que viene los tipos de los poke de la db a un array como el de la api
   let array = [];
   for (const typeName of obj.types) {
     let nueva = typeName.name;
     array.push(nueva);
-  };
+  }
   return array;
 }
-function pokeTypeFromObjToStr(poke){
+function pokeTypeFromObjToStr(poke) {
+  //para filtrar la info de los poke de la db
   return {
     id: poke.id,
     name: poke.name,
@@ -39,22 +41,25 @@ function pokeTypeFromObjToStr(poke){
     attack: poke.attack,
     defense: poke.defense,
     speed: poke.speed,
-    types: typeFilter(poke)
+    types: typeFilter(poke),
   };
-};
+}
 
 const getPokemons = async () => {
   //trae de la base de datos
-  const dbPokemonsNofilter = await Pokemon.findAll({ include: [{ model: Type , attributes:["name"] }]}); //include estaria agregando los datos de la relacion
-  
+  const dbPokemonsNofilter = await Pokemon.findAll({
+    include: [{ model: Type, attributes: ["name"] }],
+  }); //include estaria agregando los datos de la relacion
+
   const dbPokemons = [];
   for (const pokemon of dbPokemonsNofilter) {
-    dbPokemons.push(pokeTypeFromObjToStr(pokemon))
+    dbPokemons.push(pokeTypeFromObjToStr(pokemon));
   }
 
   //trae de la api
-  const apiPokemons = (await axios.get("https://pokeapi.co/api/v2/pokemon?limit=1281"))  
-    .data.results;
+  const apiPokemons = (
+    await axios.get("https://pokeapi.co/api/v2/pokemon?limit=1281")
+  ).data.results;
   const apiPokemonsUrl = await Promise.all(
     apiPokemons.map(async (element) => {
       const response = await axios.get(element.url);
@@ -66,9 +71,12 @@ const getPokemons = async () => {
 };
 
 const getPokemonsByName = async (PokeName) => {
-  let PokemonByName = await Pokemon.findOne({ where: { name: PokeName } });
+  let PokemonByName = await Pokemon.findOne({
+    where: { name: PokeName },
+    include: [{ model: Type, attributes: ["name"] }],
+  });
   if (PokemonByName) {
-    return PokemonByName;
+    return pokeTypeFromObjToStr(PokemonByName);
   } else {
     PokemonByName = await axios.get(
       `https://pokeapi.co/api/v2/pokemon/${PokeName}`
@@ -76,7 +84,6 @@ const getPokemonsByName = async (PokeName) => {
     return pokeData(PokemonByName);
   }
 };
-
 
 const getPokemonId = async (PokeId) => {
   let PokemonByID = 0;
@@ -86,8 +93,11 @@ const getPokemonId = async (PokeId) => {
     );
     return pokeData(PokemonByID);
   } else {
-    PokemonByID = await Pokemon.findOne({ where: { id: PokeId } });
-    return PokemonByID;
+    PokemonByID = await Pokemon.findOne({
+      where: { id: PokeId },
+      include: [{ model: Type, attributes: ["name"] }],
+    });
+    return pokeTypeFromObjToStr(PokemonByID);
   }
 };
 
@@ -115,9 +125,10 @@ const createPokemon = async ({
     types,
   };
   let allTypes = [];
-  for (const typeName of types) { //recorro el arreglo que llega por el post
-    const newType = await Type.findOne({where:{name: typeName}}) //busco los valores de dicho arreglo en el modelo
-    allTypes.push(newType) //los almaceno en un arreglo
+  for (const typeName of types) {
+    //recorro el arreglo que llega por el post
+    const newType = await Type.findOne({ where: { name: typeName } }); //busco los valores de dicho arreglo en el modelo
+    allTypes.push(newType); //los almaceno en un arreglo
   }
   const newPokemon = await Pokemon.create(objPokemon); // crea el nuevo pokemon
   await newPokemon.addTypes(allTypes); // asigna los tipos al nuevo pokemon
