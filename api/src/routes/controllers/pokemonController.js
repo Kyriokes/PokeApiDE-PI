c58a10f9e7a9f07908a019dae153de15b9a58181
Sergio Ferrari <ferrari8986@gmail.com>
2023-06-06 05:13:46 -0300

    fixed weird stuff ovnis

--- api/src/routes/controllers/pokemonController.js
+++ api/src/routes/controllers/pokemonController.js
@@ -60,7 +60,7 @@ const getPokemons = async () => {
 
   //trae de la api
   const apiPokemons = (
-    await axios.get("https://pokeapi.co/api/v2/pokemon?limit=251")
+    await axios.get("https://pokeapi.co/api/v2/pokemon?limit=649")
   ).data.results;
   const apiPokemonsUrl = await Promise.all(
     apiPokemons.map(async (element) => {
